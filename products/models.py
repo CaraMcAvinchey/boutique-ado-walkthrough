9eb263cbdea41a41f73db89f4e7eebff1eb41921
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-02-05 13:28:36 +0000

    Added sorting JS and back to top link

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     """
-    Model for product categories includes a programmtic name and 
+    Model for product categories includes a programmtic name and
     a friendly name for front-end.
     """
 
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -48,7 +49,8 @@ def all_products(request):
                     request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
