ff47c8f30ab0632460404a54ed05ac374437602c
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-03-10 11:48:33 +0000

    Add stripe script/elements to base.html, views.py, checkout.html, checkout.css and stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Mk3ceDaDomDLaWwSv8jafMwphkgCf5lQhmaOPFVD0otCsNt18k5R4J8XWblH9q2kwwwfV89cbtsoMUAFbd0CPfN0084hh3ko4',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
