49d2ecb575b622ca3ded0a74eb292e3ad13438ae
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2023-04-12 19:41:17 +0000

    Add cache control to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,6 +180,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # need to go to settings.py to allow Django to see the MEDIA_URL
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-cara'
     AWS_S3_REGION_NAME = 'eu-west-2'
